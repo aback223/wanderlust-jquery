654141b71ee4c262498f15b8999adbbe325c83b5
aback1227 <aback1227@gmail.com>
2018-02-12 00:03:44 -0500

    Users show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root'welcome#home'
+  resources :users, only: [:show]
   devise_scope :user do
     get 'register', to: 'devise/registrations#new', as: 'register'
     get 'login', to: 'devise/sessions#new', as: 'login'
