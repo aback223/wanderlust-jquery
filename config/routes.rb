13f6aeef6b03026d09ff750f5334ddd2ca12df53
aback1227 <aback1227@gmail.com>
2018-02-12 00:37:31 -0500

    add itinerary/new.html.erb and itinerary resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root'welcome#home'
   resources :users, only: [:show]
+  resources :itinerary
   devise_scope :user do
     get 'register', to: 'devise/registrations#new', as: 'register'
     get 'login', to: 'devise/sessions#new', as: 'login'
