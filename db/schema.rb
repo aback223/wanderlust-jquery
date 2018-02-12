7fa35ee2197cac9e86d4d872d9b0bc5b424ca729
aback1227 <aback1227@gmail.com>
2018-02-11 20:05:22 -0500

    Recreate users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180210054124) do
+ActiveRecord::Schema.define(version: 20180212005453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,13 +56,11 @@ ActiveRecord::Schema.define(version: 20180210054124) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password_digest"
-    t.string   "username"
-    t.string   "firstname"
-    t.string   "lastname"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string "email"
+    t.string "password_digest"
+    t.string "username"
+    t.string "firstname"
+    t.string "lastname"
   end
 
 end
