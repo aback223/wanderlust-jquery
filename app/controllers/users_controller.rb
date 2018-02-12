4ca9619b3907a6c912bccc414db07344be7204b0
aback1227 <aback1227@gmail.com>
2018-02-11 20:06:46 -0500

    Add user_params and create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,25 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def user_params
+    if params[:book].nil? || params[:book].empty?
+      return false
+    else
+      params.require(:user).permit(:email, :password, :firstname, :lastname, :username)
+    end
   end
 end
