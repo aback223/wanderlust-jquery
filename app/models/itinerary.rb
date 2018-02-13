7fedba388ca7748ac4a7d9ba0837fc3e8fffccf4
aback1227 <aback1227@gmail.com>
2018-02-12 21:51:19 -0500

    add associations between itinerary and user

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,2 +1,4 @@
 class Itinerary < ApplicationRecord
+  has_many :attendee_lists
+  has_many :users, through: :attendee_lists
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
   validates :username, presence: true
   validates :firstname, presence: true
   validates :lastname, presence: true
+  has_many :attendee_lists
+  has_many :itineraries, through: :attendee_lists
 end
