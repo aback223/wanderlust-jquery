036070c8f1fa63b934a439cb63bdb9e08a46de09
aback1227 <aback1227@gmail.com>
2018-02-12 22:45:24 -0500

    strong params for ItineraryController

--- app/controllers/itinerary_controller.rb
+++ app/controllers/itinerary_controller.rb
@@ -6,4 +6,10 @@ class ItineraryController < ApplicationController
   def new
     @itinerary = Itinerary.new
   end
+
+  private 
+
+  def itinerary_params
+    params.require(:itinerary).permit(:trip_title, :dateend, :datestart)
+  end
 end
