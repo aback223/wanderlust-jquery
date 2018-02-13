e5b8890a087e57bc89a4147ac1a6743e75bea80c
aback1227 <aback1227@gmail.com>
2018-02-12 22:17:45 -0500

    reroute new itinary path and add link to user show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root'welcome#home'
   resources :users, only: [:show]
-  resources :itinerary
+  resources :itinerary, except: [:new]
+  get '/users/:id/itinerary/new', to: 'itinerary#new', as: 'new_itinerary'
+
   devise_scope :user do
     get 'register', to: 'devise/registrations#new', as: 'register'
     get 'login', to: 'devise/sessions#new', as: 'login'
