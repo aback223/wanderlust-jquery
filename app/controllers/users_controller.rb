ebdb9881d408417023b1ae04500d002ffef59d63
aback1227 <aback1227@gmail.com>
2018-02-14 17:13:31 -0500

    remove itinerary index and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,17 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  def itinerary_index #users/:id/itineraries
-    @user = User.find(params[:id])
-    @itineraries = @user.itineraries
-    redirect_to user_path(@user) #users/:id
-  end
+  # def itinerary_index #users/:id/itineraries
+  #   @user = User.find(params[:id])
+  #   @itineraries = @user.itineraries
+  #   redirect_to user_path(@user) #users/:id
+  # end
 
-  def itinerary
-    @user = User.find(params[:user_id])
-    @itinerary = Itinerary.find(params[:id])
-    render template: 'users/show'
-  end
-
+  # def itinerary
+  #   @user = User.find(params[:id])
+  #   render template: 'itinerary/show'
+  # end
   def show
+    @user = User.find(params[:id])
   end
 end
