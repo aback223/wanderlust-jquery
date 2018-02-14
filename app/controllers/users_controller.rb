b7c8a99fd6ee0505ce966d544d42d99d3c7e9119
aback1227 <aback1227@gmail.com>
2018-02-13 23:23:15 -0500

    user show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,8 @@ class UsersController < ApplicationController
     @itinerary = Itinerary.find(params[:id])
     render template: 'users/show'
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
