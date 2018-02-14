dfe61df6f8ba8745d496793bd43453aea7a49865
aback1227 <aback1227@gmail.com>
2018-02-14 15:08:11 -0500

    refactor - nested resources for users and itineraries

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root'welcome#home'
 
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :itineraries
+  end
   resources :memberships, only: [:create, :show, :index]
-  resources :itinerary, except: [:new, :show, :index, :update, :destroy, :create]
   resources :invites
-  
-  get '/users/:id/itineraries/new', to: 'itinerary#new', as: 'new_itinerary'
-  get '/users/:id/itineraries', to: 'users#itinerary_index', as: 'itineraries'
-  get '/users/:id/itineraries/:itinerary_id', to: 'users#itinerary', as: 'itinerary'
-  post '/users/:id/itineraries', to: 'itinerary#create'
 
   devise_scope :user do
     get 'register', to: 'devise/registrations#new', as: 'register'
