3eca3023bec576deec9ec1fb8e6de21bfc83c0b0
aback1227 <aback1227@gmail.com>
2018-02-14 17:58:53 -0500

    remove code commented out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  # def itinerary_index #users/:id/itineraries
-  #   @user = User.find(params[:id])
-  #   @itineraries = @user.itineraries
-  #   redirect_to user_path(@user) #users/:id
-  # end
-
-  # def itinerary
-  #   @user = User.find(params[:id])
-  #   render template: 'itinerary/show'
-  # end
   def show
     @user = User.find(params[:id])
   end
