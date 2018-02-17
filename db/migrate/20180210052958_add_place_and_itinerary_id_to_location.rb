372b4d2e13558b7f12a5047c1b4d5b556f9a2bc2
aback1227 <aback1227@gmail.com>
2018-02-17 11:57:41 -0500

    delete unnecessary migration files

--- db/migrate/20180210052958_add_place_and_itinerary_id_to_location.rb
+++
@@ -1,6 +0,0 @@
-class AddPlaceAndItineraryIdToLocation < ActiveRecord::Migration[5.0]
-  def change
-    add_column :locations, :place_id, :integer
-    add_column :locations, :itinerary_id, :integer
-  end
-end
--- db/migrate/20180210054124_change_trip_to_attendee.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTripToAttendee < ActiveRecord::Migration[5.0]
-  def change
-    rename_table :trips, :attendees
-  end
-end
