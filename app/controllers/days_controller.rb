cd9072ddf8a70cbac8d46edb071ff98e47ffb6c2
aback1227 <aback1227@gmail.com>
2018-02-19 15:30:50 -0500

    add date to days, and show dates

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -6,11 +6,17 @@ class DaysController < ApplicationController
 
   def create
     @itinerary = Itinerary.find(params[:itinerary_id])
-    @day = @itinerary.days.create(title: params[:day][:title])
+    @day = @itinerary.days.create(days_params)
     if @day.save
       redirect_to itinerary_path(params[:itinerary_id])
     else
       redirect_to new_itinerary_day_path
     end
+  end
+
+  private
+
+  def days_params
+    params.require(:day).permit(:title, :date)
   end
 end
