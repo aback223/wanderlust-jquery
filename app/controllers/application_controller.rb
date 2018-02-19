2bdd2d7c4215b1b4520f989901900930c04665f7
aback1227 <aback1227@gmail.com>
2018-02-19 15:06:31 -0500

    fix permissions for edit/delete button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     user_path(current_user)
   end
-
-  def current_itinerary
-    Itinerary.find(params[:id])
-  end
-
+  
   protected
 
   def configure_permitted_parameters
