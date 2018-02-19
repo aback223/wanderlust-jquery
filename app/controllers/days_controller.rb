3f37ddcd887a8a25f4cef95c33b36c469d921794
aback1227 <aback1227@gmail.com>
2018-02-19 01:04:10 -0500

    new day form for itineraries

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,7 +1,16 @@
 class DaysController < ApplicationController
   def new
+    @day = Day.new
+    @itinerary = Itinerary.find(params[:itinerary_id])
   end
 
   def create
+    @itinerary = Itinerary.find(params[:itinerary_id])
+    @day = @itinerary.days.create(title: params[:day][:title])
+    if @day.save
+      redirect_to itinerary_path(params[:itinerary_id])
+    else
+      redirect_to new_itinerary_day_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,11 @@ Rails.application.routes.draw do
 
   resources :memberships, only: [:create, :show, :index]
   resources :invites
-  resources :days
-  
+
+  resources :itineraries, only: [:show] do
+    resources :days
+  end
+
   devise_scope :user do
     get 'register', to: 'devise/registrations#new', as: 'register'
     get 'login', to: 'devise/sessions#new', as: 'login'
