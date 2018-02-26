67d615e8495cfd0e6476173db40756354ad08580
aback1227 <aback1227@gmail.com>
2018-02-26 01:16:18 -0500

    add a create action

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,4 +3,14 @@ class ImagesController < ApplicationController
     @itinerary = Itinerary.find(params[:itinerary_id])
     @image = @itinerary.images.build
   end
+
+  def create
+    itinerary = Itinerary.find(params[:itinerary_id])
+    @image = itinerary.images.create(url: params[:image][:url])
+    if @image.save
+      redirect_to itinerary_path(itinerary)
+    else
+      render :new
+    end
+  end
 end
