c0b5617ae54048ce9d8091f35d41da8a33e87610
aback1227 <aback1227@gmail.com>
2018-02-26 00:50:18 -0500

    remove caption from images

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220181450) do
+ActiveRecord::Schema.define(version: 20180226031655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.string  "title"
+    t.string  "activity_type"
+    t.string  "address"
+    t.string  "url"
+    t.string  "notes"
+    t.integer "day_id"
+  end
 
   create_table "days", force: :cascade do |t|
     t.string  "title"
@@ -23,7 +32,6 @@ ActiveRecord::Schema.define(version: 20180220181450) do
 
   create_table "images", force: :cascade do |t|
     t.string  "url"
-    t.string  "caption"
     t.integer "itinerary_id"
     t.string  "image_num"
   end
