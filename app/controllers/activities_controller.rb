d116e3717f78b87c502f7369f9627abe4b4b3cd9
aback1227 <aback1227@gmail.com>
2018-02-26 01:20:58 -0500

    create action

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,4 +3,14 @@ class ActivitiesController < ApplicationController
     @day = Day.find(params[:day_id])
     @activity = @day.activities.build
   end
+
+  def create
+    day = Day.find(params[:day_id])
+    activity = day.activities.create(activity_params)
+    if activity.save
+      redirect_to itinerary_path(day.itinerary)
+    else
+      render :new
+    end
+  end
 end
