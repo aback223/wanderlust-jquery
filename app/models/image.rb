fd956edf27d72fa5d197985d9666ffc4a766fb5b
aback1227 <aback1227@gmail.com>
2018-02-26 00:44:52 -0500

    validation for image type

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,7 @@
 class Image < ApplicationRecord
   belongs_to :itinerary
+  validates :url, allow_blank: true, format: {
+    with: %r{\.gif|jpg|png|jpeg}, 
+    message: "must be a url for gif, jpg, or png image."
+  }
 end
