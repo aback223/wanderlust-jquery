9f2dc43a1ec59036a97a13421f35daa8c34137a9
aback1227 <aback1227@gmail.com>
2018-02-26 20:08:07 -0500

    drop places table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226031655) do
+ActiveRecord::Schema.define(version: 20180227010729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,13 +67,6 @@ ActiveRecord::Schema.define(version: 20180226031655) do
   create_table "memberships", force: :cascade do |t|
     t.integer "user_id"
     t.integer "itinerary_id"
-  end
-
-  create_table "places", force: :cascade do |t|
-    t.string   "title"
-    t.integer  "itinerary_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
   end
 
   create_table "to_do_lists", force: :cascade do |t|
