8b8da67a4d889f658c65a1dd7257ff1dea2fd981
aback1227 <aback1227@gmail.com>
2018-02-27 21:34:42 -0500

    add json render for show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @upcoming_trips = Itinerary.most_recent_by_user(@user)
+    respond_to do |format|
+      format.html {render :show}
+      format.json {render json: @user}
+    end
   end
 end
