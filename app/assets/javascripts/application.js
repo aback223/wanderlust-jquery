6f41c95d16b0c1ffb8dc45c85c8ccd1a4d5e8b1b
aback1227 <aback1227@gmail.com>
2018-02-27 22:41:35 -0500

    move javascript to assets folder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,37 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+//Click next to see next user and links to their itineraries
+
+ $(function() {
+  $(".js-next").on("click", function() {
+    var nextId = parseInt($(".js-next").attr("data-id")) + 1;
+    $.getJSON(`/users/` + nextId + ".json", function(data) {
+      $(".tripsHeader").text(`${data["firstname"]}'s Trips`);
+      $(".js-next").attr("data-id", data["id"]);
+      $(".tripsList").empty()
+      for (i=0; i < data["itineraries"].length; i++) {
+        $(".tripsList").append(
+          `
+          <h4>
+          <a href="/itineraries/${data["itineraries"][i]["id"]}">${data["itineraries"][i]["datestart"]} - ${data["itineraries"][i]["trip_title"]}</a>
+          </h4><br>
+          `
+        )
+      }
+    });
+  });
+});
+
+
+// Renders index (welcome) page and lists all itineraries
+$(function() {
+  $.getJSON("/welcome.json", function(dataset) {
+    $.each(dataset, function(k, v) {
+      $.each(v["users"], function(a,b) {
+        $('#list').prepend('<h4><a href="/itineraries/' + v["id"] + '">' + v["trip_title"] + " by: " + b["firstname"] + " " + b["lastname"] + '</h4><br>');
+      })
+    });
+  });
+});
