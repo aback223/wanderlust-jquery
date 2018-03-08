6fc8a58f67b1b39efa1909949fdab8a972a80f6a
aback1227 <aback1227@gmail.com>
2018-03-07 20:43:01 -0500

    add associations and validation to activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,15 @@
 class Activity < ApplicationRecord
   belongs_to :day
+  validates :title, presence: true
+  has_many :activity_categories
+  has_many :categories, through: :activity_categories
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      if !category_attribute["name"].empty?
+        category = Category.find_or_create_by(category_attribute)
+        self.categories << category
+      end
+    end
+  end
 end
