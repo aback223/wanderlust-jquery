3a65818692a592a6b3ef6eac19dc2c1d7c0f6f31
aback1227 <aback1227@gmail.com>
2018-03-07 20:45:19 -0500

    add validations and class method to day

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,6 +1,15 @@
 class Day < ApplicationRecord
   belongs_to :itinerary
   has_many :activities
-
   validates :date, presence: true
+  validates :date, uniqueness: true
+  validates :title, presence: true
+  validate :within_date_range, if: :date?
+
+  def within_date_range
+    itinerary = Itinerary.find_by_id(self.itinerary_id)
+    if date < itinerary.datestart || date > itinerary.dateend
+      errors.add(:date, "must be in the range: #{itinerary.datestart.strftime("%m/%d/%Y")} - #{itinerary.dateend.strftime("%m/%d/%Y")}")
+    end
+  end
 end
