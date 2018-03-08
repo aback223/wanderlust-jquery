015a1e03bd3ea625b4fbddac1bf6974224ac23e2
aback1227 <aback1227@gmail.com>
2018-03-07 21:19:32 -0500

    add itinerary helpers (show_date, image_errors, itinerary_belongs_to_user)

--- app/helpers/itineraries_helper.rb
+++ app/helpers/itineraries_helper.rb
@@ -2,4 +2,15 @@ module ItinerariesHelper
   def itinerary_belongs_to_user(user)
     @itinerary.users.include?(user)
   end
+
+  def show_date(object)
+    object.strftime("%m/%d/%Y")
+  end
+
+  def image_errors_if_exists
+    if @errors[:images]
+      @errors.delete(:images)
+      "<li>Image links need to be present with .gif/.jpg/.png format</li>".html_safe
+    end
+  end
 end
