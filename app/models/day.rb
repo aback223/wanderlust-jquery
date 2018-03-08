7cbe5c403ce509a76f46ee587670dd2b7b790979
aback1227 <aback1227@gmail.com>
2018-03-07 23:43:00 -0500

    remove validation for day uniqueness

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,7 +2,6 @@ class Day < ApplicationRecord
   belongs_to :itinerary
   has_many :activities
   validates :date, presence: true
-  validates :date, uniqueness: true
   validates :title, presence: true
   validate :within_date_range, if: :date?
 
