d1a7215db982e1bf4a416790419196341aebf77e
aback1227 <aback1227@gmail.com>
2018-03-07 20:46:50 -0500

    add validations to itinerary model

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,18 +1,15 @@
 class Itinerary < ApplicationRecord
-  has_many :comments
-  has_many :users, through: :comments
+  has_many :memberships
+  has_many :users, through: :memberships
   has_many :invites
-  validates :trip_title, presence: true
   has_many :images
   has_many :days
-  # accepts_nested_attributes_for :images
+  validates :trip_title, presence: true
+  validates :datestart, presence: true
+  validates :dateend, presence: true
+  validate :datestart_cant_be_in_past
+  validate :dateend_comes_after_datestart
   
-  def images_attributes=(images_attributes)
-    images_attributes.values.each do |image_attributes|
-      self.images.build(image_attributes)
-    end
-  end
-
   def self.most_recent_by_user(user)
     itineraries = []
     Itinerary.all.order(:datestart).each do |i|
