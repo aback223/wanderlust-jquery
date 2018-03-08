57c053836085bbbc7f3aee7a0c1b102678d945a4
aback1227 <aback1227@gmail.com>
2018-03-07 20:41:13 -0500

    change memberships join association to comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
 
   validates :firstname, presence: true
   validates :lastname, presence: true
-  has_many :memberships
-  has_many :itineraries, through: :memberships
+  has_many :comments
+  has_many :itineraries, through: :comments
   has_many :invitations, :class_name => "Invite", :foreign_key => 'recipient_id'
   has_many :sent_invites, :class_name => "Invite", :foreign_key => 'sender_id'
 
