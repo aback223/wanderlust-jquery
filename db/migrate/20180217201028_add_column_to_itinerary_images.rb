2ae45d6a7c27a4cc87e938043ac37e63384c81be
aback1227 <aback1227@gmail.com>
2018-04-04 17:15:55 -0400

    delete unused migration files

--- db/migrate/20180217201028_add_column_to_itinerary_images.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToItineraryImages < ActiveRecord::Migration[5.0]
-  def change
-    add_column :itinerary_images, :image_id, :integer
-  end
-end
--- db/migrate/20180217202349_drop_join_table.rb
+++
@@ -1,5 +0,0 @@
-class DropJoinTable < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :itinerary_images
-  end
-end
