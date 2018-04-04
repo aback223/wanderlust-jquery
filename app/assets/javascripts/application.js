a3f54c525bd7b70174e0ff46d5d49709d97c7c88
aback1227 <aback1227@gmail.com>
2018-04-04 16:47:33 -0400

    add itineraries file to js manifest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,48 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-
-//Click next to see next user and links to their itineraries
-
-function User(data) {
-  this.id = data.id
-  this.firstname = data.firstname;
-  this.itineraries = data.itineraries;
-}
-
-User.prototype.userDisplay = function() {
-  var html = "";
-  $(".tripsList").empty();
-
-  for(i=0; i<this.itineraries.length;i++){
-    html += `<h4><a href="/itineraries/${this.itineraries[i].id}">${this.itineraries[i].datestart} - ${this.itineraries[i].trip_title} </h4></a><br>`;
-  }
- 
-  $(".js-next").attr("data-id", this.id);
-  $(".tripsHeader").text(`${this.firstname}'s Trips`);
-  $(".tripsList").append(html);
-}
-
-
-
-$(function() {
-  $(".js-next").on("click", function() {
-    var nextId = parseInt($(".js-next").attr("data-id")) + 1;
-    $.getJSON(`/users/` + nextId + ".json", function(data) {
-      var user = new User(data);
-      user.userDisplay();
-    });
-  });
-});
-
-
-// Renders index (welcome) page and lists all itineraries
-$(function() {
-  $.getJSON("/welcome.json", function(dataset) {
-    $.each(dataset, function(k, v) {
-      $.each(v["users"], function(a,b) {
-        $('#list').prepend('<h4><a href="/itineraries/' + v["id"] + '">' + v["trip_title"] + " by: " + b["firstname"] + " " + b["lastname"] + '</h4><br>');
-      })
-    });
-  });
-});
+//= require itineraries
