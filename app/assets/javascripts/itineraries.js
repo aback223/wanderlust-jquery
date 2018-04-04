d6ec81b835268fb86413db57a063471f56ac8346
aback1227 <aback1227@gmail.com>
2018-04-04 18:20:38 -0400

    move js to file under assets

---
+++ app/assets/javascripts/itineraries.js
@@ -0,0 +1,22 @@
+$(function() {
+    $.getJSON("/welcome.json", function(dataset) {   
+      dataset.forEach(function(data) {
+
+        $('#list').prepend('<h4><a href="/itineraries/' + data.id + '">' + data.trip_title + " (" + data.datestart + ")" + " by: " + data.users[0].firstname + " " + data.users[0].lastname + '</h4><br>')
+      })
+    });
+  });
+
+$(function() {
+  $(".js-sort").on('click', function() {
+    $.getJSON("/welcome.json", function(dataset) {
+      const sortedData = dataset.sort((a,b) => a.datestart.localeCompare(b.datestart))
+
+      $("#list").empty();
+      sortedData.forEach(function(data) {
+
+        $('#list').append('<h4><a href="/itineraries/' + data.id + '">' + data.trip_title + " (" + data.datestart + ")" + " by: " + data.users[0].firstname + " " + data.users[0].lastname + '</h4><br>')
+      })
+    })
+  })
+})
